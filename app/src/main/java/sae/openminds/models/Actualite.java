6493bca31611e8a64429d26ce9c8be5fa8310268
Keliora <isham.dufour91@gmail.com>
2026-04-01 02:27:20 +0200

    Changement des actualitées,L'administrateur peut ajouter une actualité (avec image optionellement) et nouveau graphique + correction certains strings

--- app/src/main/java/sae/openminds/models/Actualite.java
+++ app/src/main/java/sae/openminds/models/Actualite.java
@@ -7,6 +7,7 @@ public class Actualite {
     public int    id;
     public String title;
     public String content;
+    public String image_url;      // null si pas d'image
     public String published_at;
     public String author;
 }
